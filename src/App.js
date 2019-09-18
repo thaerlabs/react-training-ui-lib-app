bf26495dc0be78c00b032915a612dec931b939bf
thaerlabs <thaer.abbas@tajawal.com>
2019-09-18 17:01:20 +0400

    theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import Button from 'demo-ui-library/lib/Button';
 import logo from './logo.svg';
 import './App.css';
 
+const tajawalTheme = {
+  primary: 'tomato',
+  secondary: 'white'
+}
+
+const almosaferTheme = {
+  primary: 'red',
+  secondary: 'yellow'
+}
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button onClick={() => {
-          console.log('The button was clicked 👍')
-        }}>
-          Learn React
-        </Button>
-      </header>
-    </div>
+    <ThemeProvider theme={tajawalTheme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Button onClick={() => {
+            console.log('The button was clicked 👍')
+          }}>
+            Learn React
+          </Button>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
